320d898f03db210fef90cdedcb97b24813077a09
Hansol Son <60804796+HansolSon1113@users.noreply.github.com>
2024-05-13 13:40:04 +0900

    Fixed clock rotating loop

--- Assets/Test/Scripts/Clock.cs
+++ Assets/Test/Scripts/Clock.cs
@@ -6,25 +6,34 @@ public class Clock : MonoBehaviour
 {
     public GameObject HourHand;
     public GameObject MinuteHand;
+    private bool isRotating = false;
+
     public void moveClock(int time)
     {
-        float currentRotation = HourHand.transform.eulerAngles.z;
-        currentRotation = Mathf.Repeat(currentRotation, 360);
-        float targetRotation = currentRotation - 15*time;
-        if (targetRotation < 0) targetRotation += 360;
-        StartCoroutine(RotateClock(targetRotation));
+        if (!isRotating)
+        {
+            StartCoroutine(RotateClock(time));
+        }
     }
 
-
-    private IEnumerator RotateClock(float targetRotation)
+    private IEnumerator RotateClock(int time)
     {
+        isRotating = true;
+
+        float currentRotation = HourHand.transform.eulerAngles.z;
+        currentRotation = Mathf.Repeat(currentRotation, 360);
+        float targetRotation = currentRotation - 15 * time;
+        if (targetRotation < 0) targetRotation += 360;
+
         while (Mathf.Abs(Mathf.DeltaAngle(HourHand.transform.eulerAngles.z, targetRotation)) > 0.01f)
         {
             HourHand.transform.Rotate(0, 0, -10 * Time.deltaTime);
             MinuteHand.transform.Rotate(0, 0, -240 * Time.deltaTime);
             yield return null;
         }
+
         MinuteHand.transform.eulerAngles = new Vector3(0, 0, 0);
+        isRotating = false;
     }
 }
 
