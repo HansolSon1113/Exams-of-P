c7c11608b54840c84a24ba5a97fa5cf1566ca74c
Zindda_ <89469092+zindalredda@users.noreply.github.com>
2024-05-19 23:01:44 +0900

    Circle day2night
    톰과 제리 엔딩 장면 오마주

--- Assets/Day/Scripts/Buttons.cs
+++ Assets/Day/Scripts/Buttons.cs
@@ -1,10 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
+    [SerializeField] GameObject BlackSqaure;
+    [SerializeField] GameObject MaskObject;
+
+    void Start() {
+        BlackSqaure.SetActive(false);
+        MaskObject.SetActive(false);
+    }
+
+    private void Day2NightCircle() {
+        BlackSqaure.SetActive(true);
+        MaskObject.SetActive(true);
+        MaskObject.transform.DOScale(new Vector3(0,0,1), 3f).OnComplete(() =>
+        {
+            SceneManager.LoadScene("Night");
+        });
+    }
+
+    private void Day2NightFade() {
+        BlackSqaure.SetActive(true);
+        Color bright = BlackSqaure.GetComponent<SpriteRenderer>().color;
+        bright.a = 1.0f;
+    }
+    
     private void OnMouseDown()
     {
         if(gameObject.name == "Night Button")
@@ -12,7 +37,7 @@ public class Buttons : MonoBehaviour
             Time.timeScale = 1f;
             Audio.Inst.playSceneChange();
             CardManager.Inst.END();
-            SceneManager.LoadScene("Night");
+            Day2NightCircle();
         }
         if(gameObject.name == "Draw Button")
         {
