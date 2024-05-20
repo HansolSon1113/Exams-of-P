12d3404b5487e31bb53c15ec0ceef2bf485729fc
Zindda_ <89469092+zindalredda@users.noreply.github.com>
2024-05-20 19:16:28 +0900

    Lobby Animation added

--- Assets/Night/Scripts/Night2Day Animation.cs
+++ Assets/Night/Scripts/Night2Day Animation.cs
@@ -9,7 +9,7 @@ public class Night2DayAnimation : MonoBehaviour
     [SerializeField] GameObject MaskObject;
 
     void Start() {
-        MaskObject.transform.DOScale(new Vector3(30,30,1), 3f).OnComplete(() =>
+        MaskObject.transform.DOScale(new Vector3(35,35,1), 3f).OnComplete(() =>
         {
             BlackSqaure.SetActive(false);
             MaskObject.SetActive(false);
