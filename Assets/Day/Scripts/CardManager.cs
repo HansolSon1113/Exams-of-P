630bdcdeb3acb2872ccc8a1abdd25dbb8c4463ef
Zindda_ <89469092+zindalredda@users.noreply.github.com>
2024-05-20 13:59:10 +0900

    Burst Animation Start
    버스트되었을 때 씬 전환 애니메이션 실행되도록 변경

--- Assets/Day/Scripts/CardManager.cs
+++ Assets/Day/Scripts/CardManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class CardManager : MonoBehaviour
 {
@@ -14,6 +15,8 @@ public class CardManager : MonoBehaviour
     [SerializeField] List<Card> cards;
     [SerializeField] Transform leftAlignment;
     [SerializeField] Transform rightAlignment;
+    [SerializeField] GameObject BlackSqaure;
+    [SerializeField] GameObject MaskObject;
     public GameObject endPanel;
     [SerializeField] GameObject MP;
     [SerializeField] GameObject burstPanel;
@@ -41,12 +44,20 @@ public class CardManager : MonoBehaviour
             END();
             clearCards(null);
             Audio.Inst.playSceneChange();
-            endPanel.SetActive(true);
-            Time.timeScale = 0;
+            Day2NightCircle();
         }
         else{
             AddCard();
         }
+    }
+
+    private void Day2NightCircle() {
+        BlackSqaure.SetActive(true);
+        MaskObject.SetActive(true);
+        MaskObject.transform.DOScale(new Vector3(0,0,1), 3f).OnComplete(() =>
+        {
+            SceneManager.LoadScene("Night");
+        });
     }
 
     //donotDestroy(카드)를 제외한 모든 카드 삭제
