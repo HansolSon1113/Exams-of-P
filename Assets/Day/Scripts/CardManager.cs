12845bf8c4b352b06390bd186902564a062e73c3
Hansol Son <60804796+HansolSon1113@users.noreply.github.com>
2024-05-21 21:51:56 +0900

    card size change

--- Assets/Day/Scripts/CardManager.cs
+++ Assets/Day/Scripts/CardManager.cs
@@ -241,7 +241,7 @@ public class CardManager : MonoBehaviour
     void CardAlignment()
     {
         List<PRS> originCardPRSs = new List<PRS>();
-        originCardPRSs = RoundAlignment(leftAlignment, rightAlignment, cards.Count, 0.5f, Vector3.one * 1f);
+        originCardPRSs = RoundAlignment(leftAlignment, rightAlignment, cards.Count, 0.5f, Vector3.one * 0.7f);
         for (int i = 0; i < cards.Count; i++)
         {
             var targetCard = cards[i];
