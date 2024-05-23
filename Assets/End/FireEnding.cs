8e8c4c66a5e85c578600d22e2d840b2f48ff9b29
Hansol Son <60804796+HansolSon1113@users.noreply.github.com>
2024-05-24 00:58:37 +0900

    fire ending anim

--- Assets/End/FireEnding.cs
+++ Assets/End/FireEnding.cs
@@ -8,6 +8,12 @@ public class FireEnding : MonoBehaviour
     [SerializeField] SpriteRenderer overPanel;
     [SerializeField] GameObject TitleButton;
     [SerializeField] GameObject RestartButton;
+    [SerializeField] GameObject doorClosed;
+    [SerializeField] GameObject doorOpened;
+    [SerializeField] GameObject character;
+    [SerializeField] Transform charLocation;
+    [SerializeField] GameObject chatSmall;
+    [SerializeField] GameObject chatBig;
 
     private IEnumerator Delay(float seconds)
     {
@@ -24,8 +30,27 @@ public class FireEnding : MonoBehaviour
         {
             // 나중에 나오는 순서, 속도, 위치 수정
             StartCoroutine(Delay(2f));
-            TitleButton.SetActive(true);
-            RestartButton.SetActive(true);
+            doorClosed.SetActive(false);
+            doorOpened.SetActive(true);
+            chatBig.SetActive(true);
+            chatBig.transform.DORotate(new Vector3(0, 0, 20), 0.15f).OnComplete(() =>
+            {
+                chatBig.transform.DORotate(new Vector3(0, 0, -20), 0.15f).OnComplete(() =>
+                {
+                    chatBig.transform.DORotate(new Vector3(0, 0, 0), 0.3f);
+                });
+            });
+            character.SetActive(true);
+            character.transform.DOMove(charLocation.position, 1f);
+            character.transform.DOScale(charLocation.localScale, 1f).OnComplete(() =>
+            {
+                chatSmall.SetActive(true);
+                StartCoroutine(Delay(1f));
+                doorOpened.SetActive(false);
+                doorClosed.SetActive(true);
+                TitleButton.SetActive(true);
+                RestartButton.SetActive(true);
+            });
         });
     }
 }
